4b664322b38ebee8d62ac708b53dbfc707fa0843
Kjumar <jganguli@hotmail.ca>
2022-02-08 16:14:23 -0500

    normally distributed random number generator

--- Assets/Scripts/Customer.cs
+++ Assets/Scripts/Customer.cs
@@ -4,14 +4,16 @@ using UnityEngine;
 
 public class Customer
 {
+    public int id { get; private set; }
     public float interarrivalTime { get; private set; }
     public float serviceTime { get; private set; }
 
-    public Customer(float interarrivalTime, float serviceTime)
+    public Customer(int id, float interarrivalTime, float serviceTime)
     {
+        this.id = id;
         this.interarrivalTime = interarrivalTime;
         this.serviceTime = serviceTime;
 
-        Debug.Log("Interarrival Time: " + interarrivalTime + "\nService Time: " + serviceTime);
+        Debug.Log("Customer id: " + id + "\nInterarrival Time: " + interarrivalTime + " | Service Time: " + serviceTime);
     }
 }
